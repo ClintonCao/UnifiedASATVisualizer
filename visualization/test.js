1502d575d7992e108b257fc2ced9e6b57c6f5efa
TimBuckers <timbuckers@gmail.com>
2016-04-26 21:03:51 +0200

    new json fomat treepam

---
+++ visualization/test.js
@@ -0,0 +1,28 @@
+var inputdata = [
+  {
+    "key": "Asia",
+    "values": [
+    {
+      "key": "India",
+      "value": 1236670000
+    },
+    {
+      "key": "China",
+      "value": 1361170000
+    }
+	]
+  },
+  {
+    "key": "Africa",
+    "values": [
+    {
+      "key": "Nigeria",
+      "value": 173615000
+    },
+    {
+      "key": "Egypt",
+      "value": 83661000
+    }
+	]
+  }
+];
