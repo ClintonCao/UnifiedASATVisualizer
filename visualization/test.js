66751afc1f8146ebdc8a43abc9a063533279634c
TimBuckers <timbuckers@gmail.com>
2016-04-26 22:15:45 +0200

    bug based colors

--- visualization/test.js
+++ visualization/test.js
@@ -1,27 +1,142 @@
 var inputdata = [
   {
-    "key": "Asia",
+    "key": "Character",
     "values": [
     {
-      "key": "India",
-      "value": 1236670000
+      "key": "Boat",
+      "value": 3,
+	  "bugs" : 5,
+	  "lines": 20,
+	  "methods": 5
     },
     {
-      "key": "China",
-      "value": 1361170000
+      "key": "Cast",
+      "value": 2,
+	  "bugs" : 5,
+	  "lines": 14,
+	  "methods": 5
+    },
+    {
+      "key": "Character",
+      "value": 2,
+	  "bugs" : 3,
+	  "lines": 3,
+	  "methods": 9
+    },
+    {
+      "key": "EnemyBoat",
+      "value": 2,
+	  "bugs" : 4,
+	  "lines": 40,
+	  "methods": 8
+    },
+    {
+      "key": "Goal",
+      "value": 2,
+	  "bugs" : 3,
+	  "lines": 40,
+	  "methods": 6
+    },
+    {
+      "key": "Cast",
+      "value": 2,
+	  "bugs" : 8,
+	  "lines": 40,
+	  "methods": 12
+    },
+    {
+      "key": "Character",
+      "value": 2,
+	  "bugs" : 3,
+	  "lines": 40,
+	  "methods": 5
+    },
+    {
+      "key": "EnemyBoat",
+      "value": 2,
+	  "bugs" : 9,
+	  "lines": 40,
+	  "methods": 14
+    },
+    {
+      "key": "Goal",
+      "value": 2,
+	  "bugs" : 0,
+	  "lines": 30,
+	  "methods": 15
+    },
+    {
+      "key": "Cast",
+      "value": 2,
+	  "bugs" : 1,
+	  "lines": 40,
+	  "methods": 12
+    },
+    {
+      "key": "Character",
+      "value": 2,
+	  "bugs" : 10,
+	  "lines": 40,
+	  "methods": 8
+    },
+    {
+      "key": "EnemyBoat",
+      "value": 2,
+	  "bugs" : 2,
+	  "lines": 40,
+	  "methods": 1
+    },
+    {
+      "key": "Goal",
+      "value": 2,
+	  "bugs" : 3,
+	  "lines": 40,
+	  "methods": 5
     }
 	]
   },
   {
-    "key": "Africa",
+    "key": "Screens",
     "values": [
     {
-      "key": "Nigeria",
-      "value": 173615000
+      "key": "Boat",
+      "value": 2,
+	  "bugs" : 10,
+	  "lines": 250,
+	  "methods": 2
     },
     {
-      "key": "Egypt",
-      "value": 83661000
+      "key": "Night",
+      "value": 2,
+	  "bugs" : 6,
+	  "lines": 40,
+	  "methods": 15
+    }
+	]
+  },
+  {
+    "key": "Movement",
+    "values": [
+    {
+      "key": "Movement",
+      "value": 2,
+	  "bugs" : 3,
+	  "lines": 30,
+	  "methods": 7
+    },
+    {
+      "key": "Random",
+      "value": 2,
+	  "bugs" : 3,
+	  "lines": 40,
+	  "methods": 3
+    },
+    {
+      "key": "Location",
+      "value": 2,
+	  "bugs" : 3,
+	  "lines": 40,
+	  "methods": 1
     }
 	]
   }
