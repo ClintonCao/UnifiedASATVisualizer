66a80c4d4245248b0fb51b8aa5c8f32931409691
boninggong <boninggong@yahoo.com>
2016-04-26 21:57:59 +0200

    More diverse example of going deep

--- visualization/test.js
+++ visualization/test.js
@@ -1,42 +1,104 @@
+/*
+Here we will show how the JSON structure should look like.
+If you want to go one level deeper, then you need to use "values" with an s on the end.
+Otherwise you can use the normal value parameter.
+*/
+
 var inputdata = [
   {
-    "key": "Asia",
-    "values": [
-    {
-      "key": "India",
-      "values": [
-        {
-          "key": "Delhi",
-          "value": 50
-        }, 
-        {
-          "key": "Mumbai",
-          "value": 60
-        }]
-    },
-    {
-      "key": "China",
-      "values": [
-        {
-          "key": "Beijing",
-          "value": 130
-        }, 
-        {
-          "key": "Shanghai",
-          "value": 80
-        }]
-    }]
+    "key": "One-deep",
+    "value": 1
   },
+
   {
-    "key": "Africa",
+    "key": "Two-deep",
     "values": [
-    {
-      "key": "Nigeria",
-      "value": 100
-    },
-    {
-      "key": "Egypt",
-      "value": 200
-    }]
+      {
+        "key": "Second-level-one",
+        "value": 0.5
+      },
+      {
+        "key": "Second-level-two",
+        "value": 0.5
+      }]
+  },
+
+  {
+    "key": "Three-deep",
+    "values": [
+      {
+        "key": "Second-level",
+        "values": [
+          {
+            "key": "Third-level-one",
+            "value": 0.5
+          },
+          {
+            "key": "Third-level-two",
+            "value": 0.5
+          }]
+      }]
+  },
+
+  {
+    "key": "Package 1",
+    "values": [
+      {
+        "key": "Class 1",
+        "values": [
+          {
+            "key": "Method 1",
+            "values": [
+              {
+                "key": "Fourth-level-one",
+                "value": 0.2
+              },
+              {
+                "key": "Fourth-level-two",
+                "value": 0.2
+              }]
+          },
+          {
+            "key": "Method 2",
+            "values": [
+              {
+                "key": "Fourth-level-one",
+                "value": 0.3
+              },
+              {
+                "key": "Fourth-level-two",
+                "value": 0.2
+              }]
+          }]
+      },
+      {
+        "key": "Class 2",
+        "values": [
+          {
+            "key": "Method 1",
+            "values": [
+              {
+                "key": "Fourth-level-one",
+                "value": 0.1
+              },
+              {
+                "key": "Fourth-level-two",
+                "value": 0
+              }]
+          },
+          {
+            "key": "Method 2",
+            "values": [
+              {
+                "key": "Fourth-level-one",
+                "value": 0.1
+              },
+              {
+                "key": "Fourth-level-two",
+                "value": 0.25
+              }]
+          }]
+      }]
   }
+
 ];
