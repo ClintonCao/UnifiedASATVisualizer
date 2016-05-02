a67c7d27253ac69db06bf3bec6b168ae4e01794f
Michiel Doesburg <M.S.Doesburg@student.tudelft.nl>
2016-05-02 10:55:44 +0200

    Added functionality: run PMD from java Processbuilder and output to new.txt in working dir.

--- src/main/java/BlueTurtle/TSE/App.java
+++ src/main/java/BlueTurtle/TSE/App.java
@@ -1,5 +1,12 @@
 package BlueTurtle.TSE;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.ProcessBuilder.Redirect;
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,20 @@ public class App
 {
     public static void main( String[] args )
     {
+    	String command = "java -jar C:\\Users\\michiel\\Desktop\\pmd-bin-4.2.6\\lib\\pmd-4.2.6.jar C:\\Users\\michiel\\workspace\\Algoritmiek\\src\\clintoncode xml basic";
+    	String c2 = "java -jar C:/Users/michiel/Desktop/pmd-bin-4.2.6/lib/pmd-4.2.6.jar C:/Users/michiel/workspace/Algoritmiek/src/clintoncode xml basic";
         System.out.println( "Hello World!" );
+        Runtime rt = Runtime.getRuntime();
+
+        ProcessBuilder pb = new ProcessBuilder("java", "-jar", "C:\\Users\\michiel\\Desktop\\pmd-bin-4.2.6\\lib\\pmd-4.2.6.jar", "C:\\Users\\michiel\\workspace\\Algoritmiek\\src\\clintoncode", "xml", "basic");
+        pb.directory(new File("C:/"));
+       
+        pb.redirectOutput(Redirect.to(new File("C:/Temp/new.txt")));
+        pb.redirectError(Redirect.INHERIT);
+        try {
+			Process p = pb.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 }
