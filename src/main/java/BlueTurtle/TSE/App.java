5e7af15f21ee66874999344c78d52650bf7b8a1e
Michiel Doesburg <M.S.Doesburg@student.tudelft.nl>
2016-05-02 12:07:11 +0200

    Added relative pathnames.

--- src/main/java/BlueTurtle/TSE/App.java
+++ src/main/java/BlueTurtle/TSE/App.java
@@ -2,33 +2,22 @@ package BlueTurtle.TSE;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.lang.ProcessBuilder.Redirect;
-import java.util.Arrays;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	String command = "java -jar C:\\Users\\michiel\\Desktop\\pmd-bin-4.2.6\\lib\\pmd-4.2.6.jar C:\\Users\\michiel\\workspace\\Algoritmiek\\src\\clintoncode xml basic";
-    	String c2 = "java -jar C:/Users/michiel/Desktop/pmd-bin-4.2.6/lib/pmd-4.2.6.jar C:/Users/michiel/workspace/Algoritmiek/src/clintoncode xml basic";
-        System.out.println( "Hello World!" );
-        Runtime rt = Runtime.getRuntime();
-
-        ProcessBuilder pb = new ProcessBuilder("java", "-jar", "C:\\Users\\michiel\\Desktop\\pmd-bin-4.2.6\\lib\\pmd-4.2.6.jar", "C:\\Users\\michiel\\workspace\\Algoritmiek\\src\\clintoncode", "xml", "basic");
-        pb.directory(new File("C:/"));
-       
-        pb.redirectOutput(Redirect.to(new File("C:/Temp/new.txt")));
+public class App {
+    public static void main( String[] args ) {
+    	String userDir = System.getProperty("user.dir");
+        ProcessBuilder pb = new ProcessBuilder("java", "-jar", userDir + "/Runnables/pmd-bin-4.2.6/lib/pmd-4.2.6.jar", userDir + "/Runnables/Testcode/ClintonCode.java", "xml", "basic");
+        pb.redirectOutput(Redirect.to(new File("./Runnables/Testcode/output.txt")));
         pb.redirectError(Redirect.INHERIT);
         try {
 			Process p = pb.start();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+        System.out.println("Done.");
     }
 }
