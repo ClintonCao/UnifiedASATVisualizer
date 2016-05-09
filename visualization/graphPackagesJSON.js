ae457c544510a0892c58ede1f6b343c9c00667d0
boninggong <B.Gong@student.tudelft.nl>
2016-05-09 10:24:59 +0200

    Better structure for JSON

--- visualization/graphPackagesJSON.js
+++ visualization/graphPackagesJSON.js
@@ -9,10 +9,10 @@
 */
 var graphJSON = {
   "nodes":[
-    {"num":0, "name":"package1", "classes":6, "loc":290, "warnings":37},
-    {"num":1, "name":"package2", "classes":2, "loc":65, "warnings":22},
-    {"num":2, "name":"package3", "classes":4, "loc":1895, "warnings":80},
-    {"num":3, "name":"package4", "classes":4, "loc":95, "warnings":27}
+    {"name":"package1", "classes":6, "loc":290, "warnings":37},
+    {"name":"package2", "classes":2, "loc":65, "warnings":22},
+    {"name":"package3", "classes":4, "loc":1895, "warnings":80},
+    {"name":"package4", "classes":4, "loc":95, "warnings":27}
   ],
   "links":[
     {"source":0, "target":1, "value":3},
