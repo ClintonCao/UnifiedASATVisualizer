8d5e6823e07b3ef63441b2a0c5b1f154cc5cd355
Sunwei Wang <S.Wang-11@student.tudelft.nl>
2016-05-09 10:00:22 +0200

    refactor the code regarding to command builder, better inheritance

--- src/main/java/BlueTurtle/TSE/CommandBuilder.java
+++ src/main/java/BlueTurtle/TSE/CommandBuilder.java
@@ -1,9 +1,29 @@
 package BlueTurtle.TSE;
 
 import BlueTurtle.interfaces.Settings;
+import java.util.ArrayList;
 
 public abstract class CommandBuilder {
+	protected ArrayList<String> commands;
+	private Settings settings;
+	
 	public abstract String[] buildCommand();
 	
-	public abstract Settings getSettings();
+	public Settings getSettings(){
+		return settings;
+	}
+	
+	public void setSettings(Settings settings) {
+		this.settings = settings;
+	}
+
+	public ArrayList<String> getCommands() {
+		return commands;
+	}
+
+	public void setCommands(ArrayList<String> commands) {
+		this.commands = commands;
+	}
+
+
 }
