6ca69b0110d31e888ef00862cbbd1b5e0239ec3c
Sunwei Wang <S.Wang-11@student.tudelft.nl>
2016-05-10 10:33:42 +0200

    write the parser for GDC

--- src/main/java/BlueTurtle/parsers/GDCParser.java
+++ src/main/java/BlueTurtle/parsers/GDCParser.java
@@ -1,12 +1,14 @@
 package BlueTurtle.parsers;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.parser.Tag;
 import org.jsoup.select.Elements;
 
 
@@ -44,12 +46,54 @@ public class GDCParser extends MarkdownParser {
 			
 			// parse the file.
 			Document doc = Jsoup.parse(inputFile, "UTF-8");
-			Element checkStyle = doc.getElementById("checkstyle-version-6.6");
+			Tag t = Tag.valueOf("dummy_tag");
+			Element checkStyle = new Element(t, "");
+			Element findbugs = new Element(t, "");
+			Element pmd = new Element(t, "");
+			// read the checkstyle table.
+			if (doc.getElementById("checkstyle-version-6.6")!= null) {
+				checkStyle = doc.getElementById("checkstyle-version-6.6");
+			}
+			// read the findbugs table.
+			if (doc.getElementById("findbugs-eclipse-preferences-version-3.0.1") != null){
+				findbugs = doc.getElementById("findbugs-eclipse-preferences-version-3.0.1").select("tbody").first();
+			}
+			// read the pmd table.
+			if (doc.getElementById("pmd-version-5.7.0") != null){
+				pmd = doc.getElementById("pmd-version-5.7.0").select("tbody").first();
+
+			}
+			Elements csRows = checkStyle.select("tr");
+			Elements fbRows = findbugs.select("tr");
+			Elements pmdRows = pmd.select("tr");
+			// append findbugs and pmd rows also in checkstyle rows.
+			csRows.addAll(fbRows);
+			csRows.addAll(pmdRows);
 			
-			Elements odds =  doc.getElementsByClass("odd");
-			Elements evens = doc.getElementsByClass("even");
-			
-			
+			// loop through each row of the entire GDC table
+			for(int i = 0; i < csRows.size(); i++){
+				Elements cols = csRows.get(i).select("td");
+				// first column is the warning name.
+				String warning = cols.get(0).text();
+				// second column is the category of warning.
+				String category = cols.get(1).text();
+				// initialize an empty warningList.
+				List<String> warningList = new ArrayList<String>();
+
+				// if the categoryInfo already has that category.
+				if(categoryInfo.containsKey(category)){
+				 // get the the current warningList
+				 warningList = categoryInfo.get(category);
+				 // add the new warning
+				 warningList.add(warning);
+				 // if the categoryInfo does not contain the current category
+				}else {
+					warningList.add(warning);
+				}
+				// update the warningList of the category.
+				categoryInfo.put(category, warningList);
+			}
+						
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
---
+++ src/test/java/BlueTurtle/parsers/GDCParserTest.java
@@ -0,0 +1,64 @@
+package BlueTurtle.parsers;
+
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import BlueTurtle.warnings.Warning;
+
+/**
+ * Test class for GDCParser.
+ * 
+ * @author BlueTurtle.
+ *
+ */
+public class GDCParserTest {
+
+	private static String testSet = "./resources/htmlExample.html";
+	private static String testSet2 = "./resources/asat-gdc-mapping.html";
+
+
+	/**
+	 * Test that the parser can parse a valid GDC html file.
+	 */
+	@Test
+	public void testParseCorrectBehaviour() {
+		GDCParser parser = new GDCParser();
+
+		HashMap<String, List<String>> categoryInfo = parser.parseFile(testSet);
+
+		assertTrue(categoryInfo.containsKey("Naming Conventions"));
+	}
+
+	/**
+	 * Test that the parser created the right amount of warnings.
+	 */
+	@Test
+	public void testCreateRightAmountOfWarnings() {
+		GDCParser parser = new GDCParser();
+
+		HashMap<String, List<String>> categoryInfo = parser.parseFile(testSet);
+
+		assertSame(1, categoryInfo.size());
+	}
+
+//	/**
+//	 * Test that the parser parse the wrong file.
+//	 */
+//	@Test
+//	public void testParseTheWrongFile() {
+//		FindBugsXMLParser parser = new FindBugsXMLParser();
+//		
+//		String testSet3 = "./resources/ex.xml";
+//
+//		List<Warning> warnings = parser.parseFile(testSet3);
+//		
+//		assertNotSame(6, warnings.toString());
+//	}
+
+}
