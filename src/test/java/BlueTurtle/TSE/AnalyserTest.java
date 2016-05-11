3fd9b6a232d01dc090dc3ced6a77b0cec51b4983
Michiel Doesburg <M.S.Doesburg@student.tudelft.nl>
2016-05-11 21:40:48 +0200

    Fixed AnalyserTest running cobertura which does not run on Travis (linux) as it is a .bat file.

--- src/test/java/BlueTurtle/TSE/AnalyserTest.java
+++ src/test/java/BlueTurtle/TSE/AnalyserTest.java
@@ -3,12 +3,21 @@ package BlueTurtle.TSE;
 import static org.junit.Assert.assertTrue;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import BlueTurtle.commandbuilders.CheckStyleCommandBuilder;
+import BlueTurtle.commandbuilders.CoberturaCommandBuilder;
+import BlueTurtle.commandbuilders.CommandBuilder;
+import BlueTurtle.commandbuilders.PMDCommandBuilder;
+import BlueTurtle.settings.CheckStyleSettings;
+import BlueTurtle.settings.PMDSettings;
 
 /**
  * Unit test for simple Analyser.
@@ -16,9 +25,28 @@ import org.junit.Test;
 public class AnalyserTest {
 	
 	@Before
+	/**
+	 * Set up a command to run PMD and run CheckStyle. These commands are handed to the analyser which runs them.
+	 * @throws IOException
+	 */
 	public void initialize() throws IOException {
-		JavaController javacontroller = new JavaController();
-		javacontroller.execute();
+		ArrayList<AnalyserCommand> commands = new ArrayList<AnalyserCommand>();
+		CommandBuilder commandBuilder;
+		PMDSettings pmdSettings = new PMDSettings();
+		commandBuilder = new PMDCommandBuilder(pmdSettings);
+		String[] pmdCommands = commandBuilder.buildCommand();
+		AnalyserCommand c1 = new AnalyserCommand(pmdSettings.getDefaultOutputFilePath(), pmdCommands);
+		commands.add(c1);
+
+		CheckStyleSettings checkStyleSettings = new CheckStyleSettings(new File("CheckStyle_Settings.xml"));
+		commandBuilder = new CheckStyleCommandBuilder(checkStyleSettings);
+		String[] checkStyleCommands = commandBuilder.buildCommand();
+		AnalyserCommand c2 = new AnalyserCommand(checkStyleSettings.getDefaultOutputFilePath(), checkStyleCommands);
+		commands.add(c2);
+
+		Analyser analyser = new Analyser(commands);
+
+		analyser.analyse();
 	}
 
 	/**
