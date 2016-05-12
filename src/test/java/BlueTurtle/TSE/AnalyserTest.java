92a8649d46758958ed7c4ebf092ac5950e8d2688
Michiel Doesburg <M.S.Doesburg@student.tudelft.nl>
2016-05-12 21:28:05 +0200

    Clinton waarom werkt dit niet.

--- src/test/java/BlueTurtle/TSE/AnalyserTest.java
+++ src/test/java/BlueTurtle/TSE/AnalyserTest.java
@@ -1,28 +1,29 @@
 package BlueTurtle.TSE;
 
 import static org.junit.Assert.assertTrue;
-
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import BlueTurtle.commandbuilders.CheckStyleCommandBuilder;
+import BlueTurtle.commandbuilders.CoberturaCommandBuilder;
 import BlueTurtle.commandbuilders.CommandBuilder;
 import BlueTurtle.commandbuilders.PMDCommandBuilder;
+import BlueTurtle.parsers.CheckStyleXMLParser;
+import BlueTurtle.parsers.XMLParser;
 import BlueTurtle.settings.CheckStyleSettings;
+import BlueTurtle.settings.CoberturaSettings;
 import BlueTurtle.settings.PMDSettings;
+import BlueTurtle.warnings.Warning;
 
 /**
  * Unit test for simple Analyser.
  */
 public class AnalyserTest {
-	ArrayList<AnalyserCommand> commands = new ArrayList<AnalyserCommand>();
 
 	@Before
 	/**
@@ -30,20 +31,24 @@ public class AnalyserTest {
 	 * @throws IOException
 	 */
 	public void initialize() throws IOException {
+		ArrayList<AnalyserCommand> commands = new ArrayList<AnalyserCommand>();
 		CommandBuilder commandBuilder;
 		PMDSettings pmdSettings = new PMDSettings();
+		CheckStyleSettings checkStyleSettings = new CheckStyleSettings(new File("CheckStyle_Settings.xml"));
+		
 		commandBuilder = new PMDCommandBuilder(pmdSettings);
 		String[] pmdCommands = commandBuilder.buildCommand();
 		AnalyserCommand c1 = new AnalyserCommand(pmdSettings.getDefaultOutputFilePath(), pmdCommands);
 		commands.add(c1);
 
-		CheckStyleSettings checkStyleSettings = new CheckStyleSettings(new File("CheckStyle_Settings.xml"));
 		commandBuilder = new CheckStyleCommandBuilder(checkStyleSettings);
 		String[] checkStyleCommands = commandBuilder.buildCommand();
 		AnalyserCommand c2 = new AnalyserCommand(checkStyleSettings.getDefaultOutputFilePath(), checkStyleCommands);
 		commands.add(c2);
 
-		
+		Analyser analyser = new Analyser(commands);
+
+		analyser.analyse();
 	}
 
 	/**
@@ -52,10 +57,9 @@ public class AnalyserTest {
 	 */
 	@Test
 	public void testCheckStyleOutput() throws IOException {
-		Analyser analyser = new Analyser(commands);
-		analyser.analyse();
-		BufferedReader br = new BufferedReader(new FileReader(JavaController.getUserDir() + "/Runnables/Testcode/checkstyle.xml"));
-		assert(br.readLine() != null);
+
+		File file = new File(JavaController.getUserDir() + "/Runnables/Testcode/checkstyle.xml");
+		assertTrue(file.length() > 0);
 	}
 	
 	/**
@@ -64,9 +68,7 @@ public class AnalyserTest {
 	 */
 	@Test
 	public void testPMDOutput() throws IOException {
-		Analyser analyser = new Analyser(commands);
-		analyser.analyse();
-		BufferedReader br = new BufferedReader(new FileReader(JavaController.getUserDir() + "/Runnables/Testcode/PMD.xml"));
-		assert(br.readLine() != null);
+		File file = new File(JavaController.getUserDir() + "/Runnables/Testcode/PMD.xml");
+		assertTrue(file.length() > 0);
 	}
 }
