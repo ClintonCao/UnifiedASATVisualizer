3f9c6725ad7f533d95b0933b2d9bdd2f11aa4cd5
Michiel Doesburg <M.S.Doesburg@student.tudelft.nl>
2016-05-14 13:07:45 +0200

    Lombok'd settings and categorysummarizer.

--- src/main/java/BlueTurtle/summarizers/CategorySummarizer.java
+++ src/main/java/BlueTurtle/summarizers/CategorySummarizer.java
@@ -1,10 +1,14 @@
 package BlueTurtle.summarizers;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 import BlueTurtle.warnings.Warning;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * This class can be used to summarize the warnings according to their categories in GDC.
@@ -12,11 +16,12 @@ import BlueTurtle.warnings.Warning;
  * @author BlueTurtle.
  *
  */
+@EqualsAndHashCode
 public class CategorySummarizer extends Summarizer {
 
-	private String category;
-	private List<Warning> warningList;
-	private HashMap<String, String> categoryInfo;
+	@Getter @Setter private String category;
+	@Getter @Setter private List<Warning> warningList;
+	@Getter @Setter private HashMap<String, String> categoryInfo;
 
 	/**
 	 * Constructor.
@@ -54,88 +59,4 @@ public class CategorySummarizer extends Summarizer {
 		}
 
 	}
-
-	
-	/**
-	 * Check whether two CategorySummarizer are equal.
-	 * 
-	 * @param other
-	 *            the other CategorySummarizer.
-	 */
-	@Override
-	public boolean equals(Object other) {
-
-		if (!(other instanceof CategorySummarizer)) {
-			return false;
-		}
-
-		CategorySummarizer that = (CategorySummarizer) other;
-		
-		return (category.equals(that.getCategory()) 
-				&& warningList.equals(that.warningList) 
-				&& numberOfWarnings == that.numberOfWarnings
-				&& categoryInfo == that.categoryInfo);
-
-	}
-
-	/**************************************/
-	/****** Getters and Setters **********/
-	/************************************/
-
-	/**
-	 * Get the category from GDC.
-	 * 
-	 * @return the name of the category.
-	 */
-	public String getCategory() {
-		return category;
-	}
-
-	/**
-	 * Set the category from GDC.
-	 * 
-	 * @param category
-	 *            the name of the category.
-	 */
-	public void setCategory(String category) {
-		this.category = category;
-	}
-
-	/**
-	 * Get the category information from GDC.
-	 * 
-	 * @return categoryInfo.
-	 */
-	public HashMap<String, String> getCategoryInfo() {
-		return categoryInfo;
-	}
-
-	/**
-	 * Set the category information from GDC.
-	 * 
-	 * @param categoryInfo
-	 *            the information table of the categories.
-	 */
-	public void setCategoryInfo(HashMap<String, String> categoryInfo) {
-		this.categoryInfo = categoryInfo;
-	}	
-	/**
-	 * Get the list of warning in this component.
-	 * 
-	 * @return list of warning in this component.
-	 */
-	public List<Warning> getWarningList() {
-		return warningList;
-	}
-
-	/**
-	 * Set the list of warnings in this component.
-	 * 
-	 * @param warnings
-	 *            list of warning.
-	 */
-	public void setWarningList(List<Warning> warnings) {
-		this.warningList = warnings;
-	}
-
 }
