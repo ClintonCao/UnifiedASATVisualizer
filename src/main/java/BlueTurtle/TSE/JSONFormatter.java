6146bfdb4189ef06492a127f7e873fba5a8e45e2
Michiel Doesburg <M.S.Doesburg@student.tudelft.nl>
2016-05-18 19:24:46 +0200

    Added gdcparser to JSONFormatter.

--- src/main/java/BlueTurtle/TSE/JSONFormatter.java
+++ src/main/java/BlueTurtle/TSE/JSONFormatter.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import BlueTurtle.finders.PackageNameFinder;
 import BlueTurtle.groupers.WarningGrouper;
 import BlueTurtle.parsers.CheckStyleXMLParser;
+import BlueTurtle.parsers.GDCParser;
 import BlueTurtle.parsers.PMDXMLParser;
 import BlueTurtle.parsers.XMLParser;
 import BlueTurtle.summarizers.Summarizer;
@@ -16,62 +17,64 @@ import BlueTurtle.warnings.Warning;
 import BlueTurtle.writers.JSWriter;
 
 public class JSONFormatter {
-	private XMLParser parser;
+	private XMLParser xmlParser;
+	private GDCParser gdcParser = new GDCParser();
+	HashMap<String, String> categoryInfo = gdcParser.parseFile("./src/main/resources/asat-gdc-mapping.html");
 	
 	public void format() throws IOException {
-		//this.parseCheckStyleXML();
-		//this.parsePMDXML();
+		this.parseCheckStyleXML();
+		this.parsePMDXML();
 	}
 	
-//	private void parseCheckStyleXML() throws IOException {
-//		parser = new CheckStyleXMLParser();
-//		
-//		List<Warning> checkStyleWarnings = parser.parseFile(JavaController.getUserDir() + "/Runnables/Testcode/checkstyle.xml");
-//
-//		System.out.println("amount of CheckStyle Warnings:" + " " + checkStyleWarnings.size());
-//
-//		for (Warning w : checkStyleWarnings) {
-//			System.out.println("Violated Rule Name: " + w.getRuleName());
-//		}
-//
-//		HashMap<String, String> componentsInfo = new HashMap<String, String>();
-//		Set<String> packagesNames = new HashSet<String>();
-//
-//		for (Warning w : checkStyleWarnings) {
-//			componentsInfo.put(w.getFileName(), w.getFilePath());
-//			packagesNames.add(PackageNameFinder.findPackageName(w.getFilePath()));
-//		}
-//
-//		WarningGrouper wg = new WarningGrouper(componentsInfo, packagesNames, checkStyleWarnings);
-//		List<Summarizer> list = wg.groupBy("packages");
-//
-//		JSWriter jwriter = new JSWriter(list);
-//		jwriter.writeToJSFormat("./src/main/resources/SummarizedOuput.js");	
-//	}
-//	
-//	private void parsePMDXML() throws IOException {
-//		parser = new PMDXMLParser();
-//		
-//		List<Warning> PMDWarnings = parser.parseFile(JavaController.getUserDir() + "/Runnables/Testcode/pmd.xml");
-//
-//		System.out.println("amount of PMD Warnings:" + " " + PMDWarnings.size());
-//
-//		for (Warning w : PMDWarnings) {
-//			System.out.println("Violated Rule Name: " + w.getRuleName());
-//		}
-//
-//		HashMap<String, String> componentsInfo = new HashMap<String, String>();
-//		Set<String> packagesNames = new HashSet<String>();
-//
-//		for (Warning w : PMDWarnings) {
-//			componentsInfo.put(w.getFileName(), w.getFilePath());
-//			packagesNames.add(PackageNameFinder.findPackageName(w.getFilePath()));
-//		}
-//
-//		WarningGrouper wg = new WarningGrouper(componentsInfo, packagesNames, PMDWarnings);
-//		List<Summarizer> list = wg.groupBy("packages");
-//
-//		JSWriter jwriter = new JSWriter(list);
-//		jwriter.writeToJSFormat("./src/main/resources/SummarizedOuput.js");	
-//	}
+	private void parseCheckStyleXML() throws IOException {
+		xmlParser = new CheckStyleXMLParser();
+		
+		List<Warning> checkStyleWarnings = xmlParser.parseFile(JavaController.getUserDir() + "/Runnables/Testcode/checkstyle.xml", categoryInfo);
+
+		System.out.println("amount of CheckStyle Warnings:" + " " + checkStyleWarnings.size());
+
+		for (Warning w : checkStyleWarnings) {
+			System.out.println("Violated Rule Name: " + w.getRuleName());
+		}
+
+		HashMap<String, String> componentsInfo = new HashMap<String, String>();
+		Set<String> packagesNames = new HashSet<String>();
+
+		for (Warning w : checkStyleWarnings) {
+			componentsInfo.put(w.getFileName(), w.getFilePath());
+			packagesNames.add(PackageNameFinder.findPackageName(w.getFilePath()));
+		}
+
+		WarningGrouper wg = new WarningGrouper(componentsInfo, packagesNames, checkStyleWarnings);
+		List<Summarizer> list = wg.groupBy("packages");
+
+		JSWriter jwriter = new JSWriter(list);
+		jwriter.writeToJSFormat("./src/main/resources/SummarizedOuput.js");	
+	}
+	
+	private void parsePMDXML() throws IOException {
+		xmlParser = new PMDXMLParser();
+		
+		List<Warning> PMDWarnings = xmlParser.parseFile(JavaController.getUserDir() + "/Runnables/Testcode/pmd.xml", categoryInfo);
+
+		System.out.println("amount of PMD Warnings:" + " " + PMDWarnings.size());
+
+		for (Warning w : PMDWarnings) {
+			System.out.println("Violated Rule Name: " + w.getRuleName());
+		}
+
+		HashMap<String, String> componentsInfo = new HashMap<String, String>();
+		Set<String> packagesNames = new HashSet<String>();
+
+		for (Warning w : PMDWarnings) {
+			componentsInfo.put(w.getFileName(), w.getFilePath());
+			packagesNames.add(PackageNameFinder.findPackageName(w.getFilePath()));
+		}
+
+		WarningGrouper wg = new WarningGrouper(componentsInfo, packagesNames, PMDWarnings);
+		List<Summarizer> list = wg.groupBy("packages");
+
+		JSWriter jwriter = new JSWriter(list);
+		jwriter.writeToJSFormat("./src/main/resources/SummarizedOuput.js");	
+	}
 }
