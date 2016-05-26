5d1635bb98dfed8cf40f9a6244dc76d77491094e
Clinton Cao <C.S.Cao@student.tudelft.nl>
2016-05-26 21:49:09 +0200

    Removed println statements.

--- src/main/java/BlueTurtle/summarizers/ComponentSummarizer.java
+++ src/main/java/BlueTurtle/summarizers/ComponentSummarizer.java
@@ -50,9 +50,6 @@ public class ComponentSummarizer extends Summarizer {
 	@Override
 	public void summarise(List<Warning> warnings) {
 		for (Warning w : warnings) {
-			System.out.println(w.getType());
-			System.out.println(w.getRuleName());
-			System.out.println(w.getFilePath());
 			String pn = PackageNameFinder.findPackageName(w.getFilePath());
 			if (w.getFileName().equals(getFileName()) && pn.equals(getPackageName())) {
 				String warningType = w.getType();
