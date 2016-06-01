6afa6f5db1c0f1edc77adb99561220f8ba82ec76
boninggong <B.Gong@student.tudelft.nl>
2016-06-01 11:17:44 +0200

    Dynamic label colours based on filter

--- visualization/helperJSFiles/handleClicks.js
+++ visualization/helperJSFiles/handleClicks.js
@@ -17,20 +17,79 @@ function handleClickTreeMapTypeSat(value, checked) {
 }
 
 /*
+ * Sets all ASAT and Categorie labels to white
+ */
+function setAllLabelsWhite() {
+	var CheckStyleElement = document.getElementById("checkStyleLabel");
+	var PMDElement = document.getElementById("PMDLabel");
+	var FindBugsElement = document.getElementById("FindBugsLabel");
+	var MaintainabilityDefectsElement = document.getElementById("maintainabilityDefectsLabel");
+	var FunctionDefectsElement = document.getElementById("functionalDefectsLabel");
+	var OtherElement = document.getElementById("otherLabel");
+	CheckStyleElement.style.color = "#FFFFFF";
+	PMDElement.style.color = "#FFFFFF";
+	FindBugsElement.style.color = "#FFFFFF";
+	MaintainabilityDefectsElement.style.color = "#FFFFFF";
+	FunctionDefectsElement.style.color = "#FFFFFF";
+	OtherElement.style.color = "#FFFFFF";
+}
+
+/*
+ * Colors based on ASAT so the labels are given colours
+ */
+function setASATColoured() {
+	var CheckStyleElement = document.getElementById("checkStyleLabel");
+	var PMDElement = document.getElementById("PMDLabel");
+	var FindBugsElement = document.getElementById("FindBugsLabel");
+	var MaintainabilityDefectsElement = document.getElementById("maintainabilityDefectsLabel");
+	var FunctionDefectsElement = document.getElementById("functionalDefectsLabel");
+	var OtherElement = document.getElementById("otherLabel");
+	CheckStyleElement.style.color = "#12B212";
+	PMDElement.style.color = "#75B4EB";
+	FindBugsElement.style.color = "#ED4337";
+	MaintainabilityDefectsElement.style.color = "#FFFFFF";
+	FunctionDefectsElement.style.color = "#FFFFFF";
+	OtherElement.style.color = "#FFFFFF";
+}
+
+/*
+ * Colors based on Categories so the labels are given colours
+ */
+function setCategoriesColoured() {
+	var CheckStyleElement = document.getElementById("checkStyleLabel");
+	var PMDElement = document.getElementById("PMDLabel");
+	var FindBugsElement = document.getElementById("FindBugsLabel");
+	var MaintainabilityDefectsElement = document.getElementById("maintainabilityDefectsLabel");
+	var FunctionDefectsElement = document.getElementById("functionalDefectsLabel");
+	var OtherElement = document.getElementById("otherLabel");
+	CheckStyleElement.style.color = "#FFFFFF";
+	PMDElement.style.color = "#FFFFFF";
+	FindBugsElement.style.color = "#FFFFFF";
+	MaintainabilityDefectsElement.style.color = "#ED4337";
+	FunctionDefectsElement.style.color = "#12B212";
+	OtherElement.style.color = "#75B4EB";
+}
+/*
  * Handles click on checkboxes for using different colorscales
  */
 function handleClickColorScale(radioButton) {
 	if ( radioButton.value == "normalAbsolute" ){
+		setAllLabelsWhite();
 		backgroundObject.setColorMethod(0);
 	}else if ( radioButton.value == "normalRelative" ){
+		setAllLabelsWhite();
 		backgroundObject.setColorMethod(1);
 	}else if ( radioButton.value == "asatAbsolute" ){
+		setASATColoured();
 		backgroundObject.setColorMethod(2);
 	}else if ( radioButton.value == "asatRelative" ){
+		setASATColoured();
 		backgroundObject.setColorMethod(3);
 	}else if ( radioButton.value == "categoryAbsolute" ){
+		setCategoriesColoured();
 		backgroundObject.setColorMethod(4);
 	}else if ( radioButton.value == "categoryRelative" ){
+		setCategoriesColoured();
 		backgroundObject.setColorMethod(5);
 	}
 }
