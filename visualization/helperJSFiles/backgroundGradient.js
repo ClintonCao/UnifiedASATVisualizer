0296b4b27327c77bb736642dfaffa91531b94dce
boninggong <B.Gong@student.tudelft.nl>
2016-06-04 19:42:34 +0200

    Fixed filter bug on all levels except the highest one

--- visualization/helperJSFiles/backgroundGradient.js
+++ visualization/helperJSFiles/backgroundGradient.js
@@ -3,7 +3,7 @@ var backgroundObject = (function() {
 	var maxConstant = 100;
 	var colorMethod = 0;
 	
-	var twoColors, color,greenTints,greenScale,blueTints, blueScale,redTints,redScale,grayTints,grayScale;
+	var twoColors, color, greenTints, greenScale, blueTints, blueScale, redTints, redScale, grayTints, grayScale, firstRatioBegin, firstRatioEnd, SecondRatioBegin, SecondRatioEnd, ThirdRatioBegin, ThirdRatioEnd, firstEdgeBegin, firstEdgeEnd, SecondEdgeBegin, SecondEdgeEnd, ThridEdgeBegin, ThirdEdgeEnd;
 	reloadColorScale();
 	
 	function reloadColorScale(){
@@ -18,7 +18,6 @@ var backgroundObject = (function() {
 		grayTints = [d3.rgb("#a2a2a2"), d3.rgb('#a2a2a2')];	
 		grayScale =  d3.scale.linear().domain([0, 0]).interpolate(d3.interpolateHcl).range(grayTints);
 	}
-	
 	/*
 	 *
 	 * Calculates the worst case of #warnings/loc
@@ -46,109 +45,195 @@ var backgroundObject = (function() {
 	function getNormalColors() {
 			return d3.scale.linear().domain([0, maxConstant]).interpolate(d3.interpolateHcl).range(twoColors);
 	}
+	function setToPercentages() {
+		firstEdgeBegin = firstRatioBegin + "%";
+		firstEdgeEnd = firstRatioEnd + "%";
+		SecondEdgeBegin = SecondRatioBegin + "%";
+		SecondEdgeEnd = SecondRatioEnd + "%";
+		ThridEdgeBegin = ThridRatioBegin + "%";
+		ThirdEdgeEnd = ThirdRatioEnd + "%";
+	}
+	function createGradientColours(gradient, onlyOneColour, whichOne, weight, ratioArray) {
+		if(onlyOneColour) {
+			switch(whichOne) {
+				case 0:
+					gradient.append("stop")
+					.attr("offset", "0%")
+					.attr("stop-color", greenScale(weight*100))
+					.attr("stop-opacity", 1);
+					
+					gradient.append("stop")
+						.attr("offset", "100%")
+						.attr("stop-color",  greenScale(weight*100))
+						.attr("stop-opacity", 1);
+					return gradient;
+				case 1:
+					gradient.append("stop")
+					.attr("offset", "0%")
+					.attr("stop-color", redScale(weight*100))
+					.attr("stop-opacity", 1);
+					
+					gradient.append("stop")
+						.attr("offset", "100%")
+						.attr("stop-color",  redScale(weight*100))
+						.attr("stop-opacity", 1);
+					return gradient;
+				case 2:
+					gradient.append("stop")
+					.attr("offset", "0%")
+					.attr("stop-color", blueScale(weight*100))
+					.attr("stop-opacity", 1);
+					
+					gradient.append("stop")
+						.attr("offset", "100%")
+						.attr("stop-color",  blueScale(weight*100))
+						.attr("stop-opacity", 1);
+					return gradient;
+				default:
+					return gradient;
+			}
+		} else {
+			if(ratioArray[0] == 0 && ratioArray[1] != 0 && ratioArray[2] != 0) {				
+				gradient.append("stop")
+					.attr("offset", "0%")
+					.attr("stop-color", redScale(weight*100))
+					.attr("stop-opacity", 1);
+				gradient.append("stop")
+					.attr("offset", SecondEdgeEnd)
+					.attr("stop-color", redScale(weight*100))
+					.attr("stop-opacity", 1);
+				ThridEdgeBegin = (SecondRatioEnd + 0.01) + "%";
+				gradient.append("stop")
+					.attr("offset", ThridEdgeBegin)
+					.attr("stop-color", blueScale(weight*100))
+					.attr("stop-opacity", 1);
+				gradient.append("stop")
+					.attr("offset", "100%")
+					.attr("stop-color", blueScale(weight*100))
+					.attr("stop-opacity", 1);
+			} else if(ratioArray[1] == 0 && ratioArray[0] != 0 && ratioArray[2] != 0) {
+				gradient.append("stop")
+					.attr("offset", "0%")
+					.attr("stop-color", greenScale(weight*100))
+					.attr("stop-opacity", 1);
+				gradient.append("stop")
+					.attr("offset", firstEdgeEnd)
+					.attr("stop-color",  greenScale(weight*100))
+					.attr("stop-opacity", 1);
+				ThridEdgeBegin = (firstRatioEnd + 0.01) + "%";
+				gradient.append("stop")
+					.attr("offset", ThridEdgeBegin)
+					.attr("stop-color", blueScale(weight*100))
+					.attr("stop-opacity", 1);
+				gradient.append("stop")
+					.attr("offset", "100%")
+					.attr("stop-color", blueScale(weight*100))
+					.attr("stop-opacity", 1);
+			} else if(ratioArray[2] == 0 && ratioArray[1] != 0 && ratioArray[0] != 0) {
+				gradient.append("stop")
+					.attr("offset", "0%")
+					.attr("stop-color", greenScale(weight*100))
+					.attr("stop-opacity", 1);
+				gradient.append("stop")
+					.attr("offset", firstEdgeEnd)
+					.attr("stop-color",  greenScale(weight*100))
+					.attr("stop-opacity", 1);
+				SecondEdgeBegin = (firstRatioEnd + 0.01) + "%";
+				gradient.append("stop")
+					.attr("offset", SecondEdgeBegin)
+					.attr("stop-color", redScale(weight*100))
+					.attr("stop-opacity", 1);
+				gradient.append("stop")
+					.attr("offset", "100%")
+					.attr("stop-color", redScale(weight*100))
+					.attr("stop-opacity", 1);
+			} else {
+				gradient.append("stop")
+					.attr("offset", firstEdgeBegin)
+					.attr("stop-color", greenScale(weight*100))
+					.attr("stop-opacity", 1);
+				gradient.append("stop")
+					.attr("offset", firstEdgeEnd)
+					.attr("stop-color",  greenScale(weight*100))
+					.attr("stop-opacity", 1);
+				gradient.append("stop")
+					.attr("offset", SecondEdgeBegin)
+					.attr("stop-color", redScale(weight*100))
+					.attr("stop-opacity", 1);
+				gradient.append("stop")
+					.attr("offset", SecondEdgeEnd)
+					.attr("stop-color", redScale(weight*100))
+					.attr("stop-opacity", 1);
+				gradient.append("stop")
+					.attr("offset", ThridEdgeBegin)
+					.attr("stop-color", blueScale(weight*100))
+					.attr("stop-opacity", 1);
+				gradient.append("stop")
+					.attr("offset", ThirdEdgeEnd)
+					.attr("stop-color", blueScale(weight*100))
+					.attr("stop-opacity", 1);
+			}
+			return gradient;
+		}
+	}
 	function calculateBackgroundGradient(svg,ratioArray,weight, id,x,y){
 		var total = ratioArray[0] + ratioArray[1] + ratioArray[2];
-		//console.log("r array: " + ratioArray);
+		
 		var tuple = gradientCalculator.calculate(x,y, ratioArray[0], ratioArray[1], ratioArray[2]);
-		var firstRatioBegin = 0;
-		var firstRatioEnd = tuple[0];
-		var SecondRatioBegin = firstRatioEnd + 0.01;
-		var SecondRatioEnd = tuple[1]
-		var ThridRatioBegin = SecondRatioEnd + 0.01;
-		var ThirdRatioEnd = 100;
+		var tupleAngle = gradientCalculator.get45Angle(x,y);
 
-		console.log(ratioArray);
+		firstRatioBegin = 0;
+		firstRatioEnd = tuple[0];
+		SecondRatioBegin = firstRatioEnd + 0.01;
+		SecondRatioEnd = tuple[1]
+		ThridRatioBegin = SecondRatioEnd + 0.01;
+		ThirdRatioEnd = 100;
 
 		if(total == 0) {
 			var gradient = svg.append("defs")
-			.append("linearGradient")
-			.attr("id", "gradient" + id)
-			.attr("spreadMethod", "pad");
-
+				.append("linearGradient")
+				.attr("id", "gradient" + id)
+				.attr("x1", "0%")
+				.attr("y1", "0%")
+				.attr("x2", "100%")
+				.attr("y2", "0%")
+				.attr("spreadMethod", "pad");
 			gradient.append("stop")
 				.attr("offset", "0%")
 				.attr("stop-color", grayScale(0))
 				.attr("stop-opacity", 1);
-				
 			gradient.append("stop")
 				.attr("offset", "100%")
 				.attr("stop-color",  grayScale(0))
 				.attr("stop-opacity", 1);
-			
 			return gradient;
-
 		} else {
+			var gradient = svg.append("defs")	
+			  	.append("linearGradient")
+				.attr("id", "gradient" + id)
+				.attr("x1", "0%")
+				.attr("y1", "0%")
+				.attr("x2", tupleAngle[0])
+				.attr("y2", tupleAngle[1])
+				.attr("spreadMethod", "pad");
 			if(ratioArray[0] == 100) {
-				SecondRatioBegin = 0;
-				SecondRatioEnd = 0;
-				ThridRatioBegin = 0;
-				ThirdRatioEnd = 0;
+				setToPercentages();
+				return createGradientColours(gradient, true, 0, weight, ratioArray);
 			} else if(ratioArray[1] == 100) {
-				ThridRatioBegin = 0;
-				ThirdRatioEnd = 0;
+				setToPercentages();
+				return createGradientColours(gradient, true, 1, weight, ratioArray);
 			} else if(ratioArray[2] == 100) {
-				SecondRatioBegin = 0;
-				SecondEdgeEnd = 0;
-				ThridRatioBegin = 0;
-				ThirdRatioEnd = 0;
+				setToPercentages();
+				return createGradientColours(gradient, true, 2, weight, ratioArray);
+			} else {
+				setToPercentages();
+				return createGradientColours(gradient, false, -1, weight, ratioArray);
 			}
 		}
-		
-		var firstEdgeBegin = firstRatioBegin + "%";
-		var firstEdgeEnd = firstRatioEnd + "%";
-		var SecondEdgeBegin = SecondRatioBegin + "%";
-		var SecondEdgeEnd = SecondRatioEnd + "%";
-		var ThridEdgeBegin = ThridRatioBegin + "%";
-		var ThirdEdgeEnd = ThirdRatioEnd + "%";
-
-		console.log(firstEdgeEnd);
-		console.log(SecondEdgeEnd);
-		console.log(ThirdEdgeEnd);
-
-	   	var tupleAngle = gradientCalculator.get45Angle(x,y);
-       	var gradient = svg.append("defs")	
-					  	.append("linearGradient")
-						.attr("id", "gradient" + id)
-						.attr("x1", "0%")
-						.attr("y1", "0%")
-						.attr("x2", tupleAngle[0])
-						.attr("y2", tupleAngle[1])
-						.attr("spreadMethod", "pad");
-
-	gradient.append("stop")
-		.attr("offset", firstEdgeBegin)
-		.attr("stop-color", greenScale(weight*100))
-		.attr("stop-opacity", 1);
-		
-	gradient.append("stop")
-		.attr("offset", firstEdgeEnd)
-		.attr("stop-color",  greenScale(weight*100))
-		.attr("stop-opacity", 1);
-		
-	gradient.append("stop")
-		.attr("offset", SecondEdgeBegin)
-		.attr("stop-color", redScale(weight*100))
-		.attr("stop-opacity", 1);
-	
-	gradient.append("stop")
-		.attr("offset", SecondEdgeEnd)
-		.attr("stop-color", redScale(weight*100))
-		.attr("stop-opacity", 1);
-		
-	gradient.append("stop")
-		.attr("offset", ThridEdgeBegin)
-		.attr("stop-color", blueScale(weight*100))
-		.attr("stop-opacity", 1);
-		
-	gradient.append("stop")
-		.attr("offset", ThirdEdgeEnd)
-		.attr("stop-color", blueScale(weight*100))
-		.attr("stop-opacity", 1);
-		return gradient;
 	}
 	function checkIfNot0(number) {
 		if(number == 0) {
-			return 0.000001;
+			return 0.1;
 		} else {
 			return number;
 		}
@@ -203,43 +288,28 @@ var backgroundObject = (function() {
                 var ratioCheckStyle = Math.round(constant * d.warningsCheckStyle / warningsTotal);	
 				if ( ratioCheckStyle > maxConstant ) { ratioCheckStyle = maxConstant; }
 				ratioArrayASAT.push(ratioCheckStyle);
-				//console.log("---------------------------CS------------------------");
-				//console.log(d.warningsCheckStyle);
-				//console.log(ratioArrayASAT);
+				
                 var ratiowarningsPMD = Math.round(constant * d.warningsPMD / warningsTotal);	
 				if ( ratiowarningsPMD > maxConstant ) { ratiowarningsPMD = maxConstant; }
 				ratioArrayASAT.push(ratiowarningsPMD);
-				//console.log("---------------------------PMD------------------------");
-				//console.log(d.warningsPMD);
-				//console.log(ratioArrayASAT);
+				
                 var ratioFindBugs = Math.round(constant * d.warningsFindBugs / warningsTotal);	
 				if ( ratioFindBugs > maxConstant ) { ratioFindBugs = maxConstant; }
 				ratioArrayASAT.push(ratioFindBugs);
-				//console.log("---------------------------FB------------------------");
-				//console.log(d.warningsFindBugs);
-				//console.log(ratioArrayASAT);
 
 				ratioArrayCategory = [];
                 var ratioFunctionalDefects = Math.round(constant * d.warningsFunctionalDefects / warningsTotal);	
 				if ( ratioFunctionalDefects > maxConstant ) { ratioFunctionalDefects = maxConstant; }
 				ratioArrayCategory.push(ratioFunctionalDefects);
-				//console.log("---------------------------Func------------------------");
-				//console.log(d.warningsFunctionalDefects);
-				//console.log(ratioArrayCategory);
 				
                 var ratioMaintainabilityDefects = Math.round(constant * d.warningsMaintainabilityDefects / warningsTotal);	
 				if ( ratioMaintainabilityDefects > maxConstant ) { ratioMaintainabilityDefects = maxConstant; }
 				ratioArrayCategory.push(ratioMaintainabilityDefects);
-				//console.log("---------------------------Main------------------------");
-				//console.log(d.warningsMaintainabilityDefects);
-				//console.log(ratioArrayCategory);
 				
                 var ratioOtherDefects = Math.round(constant * d.warningsOtherDefects / warningsTotal);	
 				if ( ratioOtherDefects > maxConstant ) { ratioOtherDefects = maxConstant; }
 				ratioArrayCategory.push(ratioOtherDefects);
-				//console.log("---------------------------Other------------------------");
-				//console.log(d.warningsOtherDefects);
-				//console.log(ratioArrayCategory);
+			
 				return [ratioArrayASAT,ratioArrayCategory]
 		}
     }
