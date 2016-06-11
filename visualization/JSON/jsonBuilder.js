641d370ed9858999182bdb7b71849b3532685c75
boninggong <B.Gong@student.tudelft.nl>
2016-06-11 13:01:15 +0200

    Cleaned up the html file

--- visualization/JSON/jsonBuilder.js
+++ visualization/JSON/jsonBuilder.js
@@ -4,15 +4,6 @@
 
 /*
  *
- * Replace \ for / in path for usage
- *
- */ 
-function replaceAll(stringObject, target, replacement){
-	return stringObject.split(target).join(replacement);
-}
-
-/*
- *
  * Filter on type of tool and/or warnings
  *
  */
@@ -272,69 +263,8 @@ function createJsonTreeMap(packages){
 }
 
 /*
- *
- * Creates a JSON file that could be used by the graph for package level
- *
- */
-function createJsonGraphPackages(packages){
-	var jsonArrPackage = [];
-	var linksArray = [];
- 	for(var p =0; p < packages.length; p++){
- 		for(var z = p+1; z < packages.length; z++) {
- 			if((Math.floor(Math.random() * 10) + 1) <= 8) {
- 				linksArray.push({"source":p, "target":z, "value": 10});
- 			}
- 		}
-	  	var jsonArrClass = [];
-	  	var classes = packages[p];
-	  	var totalWarningsPackage = 0;
-	  	var numberOfClasses = 0;
-	  	var totalLines = 0;
-      	for (var i = 0; i < classes.length; i++) {
-        	var fileName = classes[i].fileName;
-        	var linesOfCode = classes[i].loc;
-        	var amountOfWarnings = classes[i].amountOfWarnings;
-        	totalWarningsPackage += amountOfWarnings;
-        	totalLines += classes[i].loc;
-        	numberOfClasses++;
-        	jsonArrClass.push({
-          	fileName: fileName,
-          	loc: linesOfCode,
-          	warnings: amountOfWarnings
-        	});
-      	}
-      	jsonArrPackage.push({fileName: classes.packageName, numberOfClasses: numberOfClasses, totalWarnings:totalWarningsPackage, loc:totalLines, classes: jsonArrClass});
-	}
-	return {nodes: jsonArrPackage, links: linksArray };
-}
-
-/*
- *
- * Creates a JSON file that could be used by the graph for project level
- *
- */
-function createJsonGraphClasses(packages, packageName){
- 	for(var p =0; p < packages.length; p++){
-    	if(packages[p].packageName == packageName) {
-      	var jsonArrClass = [];
-      	var linksArray = [];
-      	var classes = packages[p];
-	      	for (var i = 0; i < classes.length; i++) {
-	      		for(var z = i+1; z < classes.length; z++) {
-	      			if((Math.floor(Math.random() * 10) + 1) <= 2) {
-		 				linksArray.push({"source":i, "target":z, "value": 10});
-		 			}
-		 		}
-	        	var fileName = classes[i].fileName;
-	        	var linesOfCode = classes[i].loc;
-	        	var amountOfWarnings = classes[i].amountOfWarnings;
-	        	jsonArrClass.push({
-	          	fileName: fileName,
-	          	loc: linesOfCode,
-	          	warnings: amountOfWarnings
-	        	});
-	      	}
-    	}
-  	}
-	return {nodes: jsonArrClass, links: linksArray };
+ * Replace \ for / in path for usage
+ */ 
+function replaceAll(stringObject, target, replacement){
+	return stringObject.split(target).join(replacement);
 }
