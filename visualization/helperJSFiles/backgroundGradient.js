dffb2226e476a5fd516558bc56451d8f0959a39e
boninggong <B.Gong@student.tudelft.nl>
2016-06-12 11:47:23 +0200

    Refactored backgroundGradient.js

--- visualization/helperJSFiles/backgroundGradient.js
+++ visualization/helperJSFiles/backgroundGradient.js
@@ -5,7 +5,10 @@ var backgroundObject = (function() {
 	var firstRatioBegin, firstRatioEnd, SecondRatioBegin, SecondRatioEnd, ThirdRatioBegin, ThirdRatioEnd, firstEdgeBegin, firstEdgeEnd, SecondEdgeBegin, SecondEdgeEnd, ThridEdgeBegin, ThirdEdgeEnd;
 	reloadColorScale();
 	
-	function reloadColorScale(){
+	/*
+	 * Defines all different colour scales
+	 */
+	function reloadColorScale() {
 		twoColors = [d3.rgb(colours.lightGreen()), d3.rgb(colours.darkRed())];
 		color = d3.scale.linear().domain([0, maxConstant]).interpolate(d3.interpolateHcl).range(twoColors);
 		greenTints = [d3.rgb(colours.lightGreen()), d3.rgb(colours.darkGreen())];
@@ -60,7 +63,7 @@ var backgroundObject = (function() {
 			.attr("stop-color", greenScale(weight*100))
 			.attr("stop-opacity", 1);
 	}
-	function setRedScale(gradient, start, end, weight) {
+	function setOrangeScale(gradient, start, end, weight) {
 		gradient.append("stop")
 			.attr("offset", start)
 			.attr("stop-color", orangeScale(weight*100))
@@ -80,6 +83,16 @@ var backgroundObject = (function() {
 			.attr("stop-color", blueScale(weight*100))
 			.attr("stop-opacity", 1);
 	}
+	function setNormalScale(gradient) {
+		gradient.append("stop")
+				.attr("offset", "0%")
+				.attr("stop-color", grayScale(0))
+				.attr("stop-opacity", 1);
+			gradient.append("stop")
+				.attr("offset", "100%")
+				.attr("stop-color",  grayScale(0))
+				.attr("stop-opacity", 1);
+	}
 	function createGradientColours(gradient, onlyOneColour, whichOne, weight, ratioArray) {
 		if(onlyOneColour) {
 			switch(whichOne) {
@@ -87,7 +100,7 @@ var backgroundObject = (function() {
 					setGreenScale(gradient, "0%", "100%", weight);
 					return gradient;
 				case 1:
-					setRedScale(gradient, "0%", "100%", weight);
+					setOrangeScale(gradient, "0%", "100%", weight);
 					return gradient;
 				case 2:
 					setBlueScale(gradient, "0%", "100%", weight);
@@ -97,7 +110,7 @@ var backgroundObject = (function() {
 			}
 		} else {
 			if(ratioArray[0] == 0 && ratioArray[1] != 0 && ratioArray[2] != 0) {			
-				setRedScale(gradient, "0%", SecondEdgeEnd, weight);
+				setOrangeScale(gradient, "0%", SecondEdgeEnd, weight);
 				ThridEdgeBegin = (SecondRatioEnd + 0.01) + "%";
 				setBlueScale(gradient, ThridEdgeBegin, "100%", weight);
 			} else if(ratioArray[1] == 0 && ratioArray[0] != 0 && ratioArray[2] != 0) {
@@ -107,18 +120,17 @@ var backgroundObject = (function() {
 			} else if(ratioArray[2] == 0 && ratioArray[1] != 0 && ratioArray[0] != 0) {
 				setGreenScale(gradient, "0%", firstEdgeEnd, weight);
 				SecondEdgeBegin = (firstRatioEnd + 0.01) + "%";
-				setRedScale(gradient, SecondEdgeBegin, "100%", weight);
+				setOrangeScale(gradient, SecondEdgeBegin, "100%", weight);
 			} else {
 				setGreenScale(gradient, "0%", firstEdgeEnd, weight)
-				setRedScale(gradient, SecondEdgeBegin, SecondEdgeEnd, weight);
+				setOrangeScale(gradient, SecondEdgeBegin, SecondEdgeEnd, weight);
 				setBlueScale(gradient, ThridEdgeBegin, "100%", weight);
 			}
 			return gradient;
 		}
 	}
-	function calculateBackgroundGradient(svg,ratioArray,weight, id,x,y){
+	function calculateBackgroundGradient(svg,ratioArray,weight, id,x,y) {
 		var total = ratioArray[0] + ratioArray[1] + ratioArray[2];
-		
 		var tuple = gradientCalculator.calculate(x,y, ratioArray[0], ratioArray[1], ratioArray[2]);
 		var tupleAngle = gradientCalculator.get45Angle(x,y);
 
@@ -129,56 +141,43 @@ var backgroundObject = (function() {
 		ThridRatioBegin = SecondRatioEnd + 0.01;
 		ThirdRatioEnd = 100;
 
+		var y2 = "100%";
 		if(total == 0) {
-			var gradient = svg.append("defs")
+			y2 = "0%";
+		}
+		var gradient = svg.append("defs")
 				.append("linearGradient")
 				.attr("id", "gradient" + id)
 				.attr("x1", "0%")
 				.attr("y1", "0%")
 				.attr("x2", "100%")
-				.attr("y2", "0%")
+				.attr("y2", y2)
 				.attr("spreadMethod", "pad");
-			gradient.append("stop")
-				.attr("offset", "0%")
-				.attr("stop-color", grayScale(0))
-				.attr("stop-opacity", 1);
-			gradient.append("stop")
-				.attr("offset", "100%")
-				.attr("stop-color",  grayScale(0))
-				.attr("stop-opacity", 1);
+
+		if(total == 0) {
+			setNormalScale(gradient);
 			return gradient;
 		} else {
-			var gradient = svg.append("defs")	
-			  	.append("linearGradient")
-				.attr("id", "gradient" + id)
-				.attr("x1", "0%")
-				.attr("y1", "0%")
-				.attr("x2", "100%")
-				.attr("y2", "100%")
-				.attr("spreadMethod", "pad");
+			setToPercentages();
 			if(ratioArray[0] == 100) {
-				setToPercentages();
 				return createGradientColours(gradient, true, 0, weight, ratioArray);
 			} else if(ratioArray[1] == 100) {
-				setToPercentages();
 				return createGradientColours(gradient, true, 1, weight, ratioArray);
 			} else if(ratioArray[2] == 100) {
-				setToPercentages();
 				return createGradientColours(gradient, true, 2, weight, ratioArray);
 			} else {
-				setToPercentages();
 				return createGradientColours(gradient, false, -1, weight, ratioArray);
 			}
 		}
 	}
-	function checkIfNot0(number) {
+	function checkIfNotZero(number) {
 		if(number == 0) {
 			return 0.1;
 		} else {
 			return number;
 		}
 	}
-	function calculateBackground(svg, weight, id){
+	function calculateBackground(svg, weight, id) {
 		var currentColorScale = getNormalColors();
 		var gradient = svg.append("defs")
 			.append("linearGradient")
@@ -209,39 +208,34 @@ var backgroundObject = (function() {
 					break;
 			} 
 		},
-		setColorsRelative: function(){
+		setColorsRelative: function() {
 			maxConstant = getRelativeWarnings();
 			reloadColorScale();
 		},
-		setColorsAbsolute: function(){
+		setColorsAbsolute: function() {
 			maxConstant = 100;
 			reloadColorScale();
 		},
-		setColorMethod: function(index){
+		setColorMethod: function(index) {
 			colorMethod = index;
 		},
-		getRatios: function(d){
-			ratioArrayASAT = [];
+		getRatios: function(d) {
 			var constant = 100;
-			var warningsTotal = checkIfNot0(d.warnings);
+			var warningsTotal = checkIfNotZero(d.warnings);
 
+			ratioArrayASAT = [];
             var ratioCheckStyle = Math.round(constant * d.warningsCheckStyle / warningsTotal);	
 			ratioArrayASAT.push(ratioCheckStyle);
-			
             var ratiowarningsPMD = Math.round(constant * d.warningsPMD / warningsTotal);	
 			ratioArrayASAT.push(ratiowarningsPMD);
-			
             var ratioFindBugs = Math.round(constant * d.warningsFindBugs / warningsTotal);	
 			ratioArrayASAT.push(ratioFindBugs);
 			
 			ratioArrayCategory = [];
-			
             var ratioMaintainabilityDefects = Math.round(constant * d.warningsMaintainabilityDefects / warningsTotal);	
 			ratioArrayCategory.push(ratioMaintainabilityDefects);
-			
             var ratioOtherDefects = Math.round(constant * d.warningsOtherDefects / warningsTotal);	
 			ratioArrayCategory.push(ratioOtherDefects);
-		
 			var ratioFunctionalDefects = Math.round(constant * d.warningsFunctionalDefects / warningsTotal);	
 			ratioArrayCategory.push(ratioFunctionalDefects);
 
